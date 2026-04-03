b5df5a0deb1fb141b01c317be17e4377d344c2fa
cprasad <cprasad@rythmos.com>
2026-04-03 15:23:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log("MongoDB connection error:", err));
 
 // ===== MODELS =====
 const User = mongoose.model("User", { username: String, password: String, avatar: String });
